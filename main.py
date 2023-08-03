2a09eb464589ab805041a6ca47d434bd83fa9460
Flowmikro <cloplop091@gmail.com>
2023-08-04 00:09:03 +0300

    start-html/main/login

--- main.py
+++ main.py
@@ -1,4 +1,4 @@
-from flask import Flask, render_template, flash, request
+from flask import Flask, render_template, flash, request, session, redirect, url_for, abort
 
 # render_template - чтобы templates подгружал
 # flash это типа ответ юзеру на взаимодействие с интерфейсом
@@ -8,7 +8,6 @@ app = Flask(__name__)
 app.config['SECRET_KEY'] = 'sdfq4gv34qhgbvqqqqqqqqba'
 # секретный ключ типа как {% csrf_token %}
 # значение придумываем сами
-
 manu = [
     {'name': 'Установка', 'url': 'install-flask'},
     {'name': 'Приложение', 'url': 'start-app'},
@@ -31,14 +30,31 @@ def contact():
     return render_template('contact.html')
 
 
-@app.route('/name/<username>')  # динамический url
-def username(username):  # передаем аргумент
-    return f'Имя: {username}'  # например:   http://127.0.0.1:5000/name/islam -> Имя: islam
+@app.route('/login', methods=['GET', 'POST'])  # прописываем регистрацию
+def login():
+    if 'userLogged' in session:  # исправляем синтаксис
+        return redirect(url_for('profile', username=session['userLogged']))
+    elif request.method == 'POST' and request.form['username'] == 'islam' and request.form['psw'] == '1234':
+        session['userLogged'] = request.form['username']
+        return redirect(url_for('profile', username=session['userLogged']))  # исправляем опечатку
+    return render_template('login.html')
+
+
+@app.route('/pofile/<username>')  # динамический url
+def profile(username):  # передаем аргумент
+    if 'userLogged' not in session or session['userLogged'] != username:
+        abort(401)
+
+    return f'Имя: {username}'  # например:   http://127.0.0.1:5000/pofile/islam -> Имя: islam
 
 
 # @app.route('/info')
 # def info():
 #     return '<h1>Инфо о сайте</h1>'  # так не вариант делать, по это templates делаем
+
+@app.errorhandler(404)  # ловим ошибку 404
+def page_not_fount(error):
+    return render_template('page404.html'), 404
 
 
 if __name__ == '__main__':
