b16ba5be6d4b0ac6fe3d2da73c4dbcf9f8f8d3e5
Flowmikro <cloplop091@gmail.com>
2023-08-03 21:47:15 +0300

    start-css

--- main.py
+++ main.py
@@ -2,15 +2,21 @@ from flask import Flask, render_template  # render_template - чтобы templat
 
 app = Flask(__name__)
 
+manu = [
+    {'name': 'Установка', 'url': 'install-flask'},
+    {'name': 'Приложение', 'url': 'start-app'},
+    {'name': 'Контакты', 'url': 'contact'},
+]
+
 
 @app.route('/')  # с помощью декоратора прописываем url
 def index():
-    return render_template('index.html')
+    return render_template('index.html', menu=manu)
 
 
 @app.route('/name/<username>')  # динамический url
 def username(username):  # передаем аргумент
-    return f'Имя: {username}'  # например:  http://127.0.0.1:5000/name/islam -> Имя: islam
+    return f'Имя: {username}'  # например:   http://127.0.0.1:5000/name/islam -> Имя: islam
 
 
 # @app.route('/info')
